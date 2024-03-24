1bab4fd6c905c30bd2f5801b8223436330711970
liuzhongao <1047771038@qq.com>
2024-03-25 00:39:48 +0800

    publish configuration change.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
-include(":jvm-only-module")
-
 pluginManagement {
     repositories {
         maven { url = uri("${rootProject.projectDir}/repo") }
@@ -22,3 +20,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "ProjectName"
 include(":android-only-module")
+include(":jvm-only-module")
